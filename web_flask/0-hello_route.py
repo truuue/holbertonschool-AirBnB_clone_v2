52ada0a9d0c28e12ce20a51040822c38d13b6ea1
truuue <noahvernhet@gmail.com>
2023-08-14 02:47:47 -0700

    More comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+"""Script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """module that display Hello HBNB!"""
+    """fonction that display Hello HBNB! by called /"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
