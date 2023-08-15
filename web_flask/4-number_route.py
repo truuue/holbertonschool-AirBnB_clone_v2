d5d1e79f4c1a83509f33d544897a8693d15ab4a8
truuue <noahvernhet@gmail.com>
2023-08-15 07:44:16 -0700

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """fonction that display Hello HBNB! by called /"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """fonction that display HBNB by called /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """fonction that display C and the value of <text> by called /c/<text>"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """fonction that display Python and the value of <text>
+    by called /python/<text>"""
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def n_text(n):
+    """fonction that display n is a number only if n is an int
+    by called /number/<n>"""
+    if n is type(int):
+        return 'n is a number'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
