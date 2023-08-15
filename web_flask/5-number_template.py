1f6420d070e6151a8916dcb0ce23fd4e736b5325
truuue <noahvernhet@gmail.com>
2023-08-15 07:57:49 -0700

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """fonction that display Hello HBNB! by called /"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """fonction that display HBNB by called /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """fonction that display C and the value of <text> by called /c/<text>"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """fonction that display Python and the value of <text>
+    by called /python/<text>"""
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_text(n):
+    """fonction that display n is a number only if n is an int
+    by called /number/<n>"""
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def n_html(n):
+    """fonction that display a HTML page only if n is an int
+    by called /number/<n>"""
+    return n_html('5-number.html', num=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
