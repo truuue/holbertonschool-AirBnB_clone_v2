5385a7a4dab704016395e2ce630af6863e88e007
truuue <noahvernhet@gmail.com>
2023-08-18 12:14:58 -0700

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,58 +1,64 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-from flask import Flask
-from flask import render_template
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
 from models import *
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """fonction that display Hello HBNB! by called /"""
+def hello_hbnb():
+    """ Function called with / route """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """fonction that display HBNB by called /hbnb"""
+    """ Function called with /hbnb route """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """fonction that display C and the value of <text> by called /c/<text>"""
-    text = text.replace('_', ' ')
-    return f'C {text}'
+    """
+    Function called with /c/<text> route
+    display C followed by text variable
+    """
+    return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """fonction that display Python and the value of <text>
-    by called /python/<text>"""
-    text = text.replace('_', ' ')
-    return f'Python {text}'
+    """
+    Function called with /python/<text> route
+    display Python followed by text variable
+    """
+    return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def n_text(n):
-    """fonction that display n is a number only if n is an int
-    by called /number/<n>"""
-    return f'{n} is a number'
+def number(n):
+    """
+    Function called with /number/<n> route
+    """
+    return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def n_html(n):
-    """fonction that display a HTML page only if n is an int
-    by called /number/<n>"""
+def number_template(n):
+    """
+    Function called with /number_template/<int:n> route
+    """
     return render_template('5-number.html', num=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    choise = "odd" if n % 2 != 0 else "even"
-    return render_template('6-number_odd_or_even.html', num=n, choise=choise)
+    """
+    Function called with /number_odd_or_even/<int:n> route
+    """
+    return render_template('6-number_odd_or_even.html', num=n)
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -70,5 +76,6 @@ def teardown(err):
     """remove the current session"""
     storage.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
